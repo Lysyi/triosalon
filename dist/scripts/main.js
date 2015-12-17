276a8a63f140be2e5a2ae12991af9cff8a2d48da
Sasha <oleksandr.lysyi@gmail.com>
2015-12-17 17:35:07 +0200

    favico, changed map bg, added color socIcons etc

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -2,21 +2,21 @@
 
 function initialize() {
   'use strict';
-  var styles = [{
-    'featureType': 'all',
-    'elementType': 'labels.text.fill',
-    'stylers': [{'saturation': 36}, {'color': '#000000'}, {'lightness': 40}]}, {'featureType': 'all', 'elementType': 'labels.text.stroke', 'stylers': [{'visibility': 'on'}, {'color': '#000000'}, {'lightness': 16}]}, {'featureType': 'all', 'elementType': 'labels.icon', 'stylers': [{'visibility': 'off'}]}, {'featureType': 'administrative', 'elementType': 'geometry.fill', 'stylers': [{'color': '#000000'}, {'lightness': 20}]}, {'featureType': 'administrative', 'elementType': 'geometry.stroke', 'stylers': [{'color': '#000000'}, {'lightness': 17}, {'weight': 1.2}]}, {'featureType': 'landscape', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 20}]}, {'featureType': 'poi', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 21}]}, {'featureType': 'road.highway', 'elementType': 'geometry.fill', 'stylers': [{'color': '#000000'}, {'lightness': 17}]}, {'featureType': 'road.highway', 'elementType': 'geometry.stroke', 'stylers': [{'color': '#000000'}, {'lightness': 29}, {'weight': 0.2}]}, {'featureType': 'road.arterial', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 18}]}, {'featureType': 'road.local', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 16}]}, {'featureType': 'transit', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 19}]}, {'featureType': 'water', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 17}]}];
-  var styledMap = new google.maps.StyledMapType(styles,
-    {name: 'Styled Map'});
-  var bangalore = { lat: 41.896374, lng: -87.627070 };
+  // var styles = [{
+  //   'featureType': 'all',
+  //   'elementType': 'labels.text.fill',
+  //   'stylers': [{'saturation': 36}, {'color': '#000000'}, {'lightness': 40}]}, {'featureType': 'all', 'elementType': 'labels.text.stroke', 'stylers': [{'visibility': 'on'}, {'color': '#000000'}, {'lightness': 16}]}, {'featureType': 'all', 'elementType': 'labels.icon', 'stylers': [{'visibility': 'off'}]}, {'featureType': 'administrative', 'elementType': 'geometry.fill', 'stylers': [{'color': '#000000'}, {'lightness': 20}]}, {'featureType': 'administrative', 'elementType': 'geometry.stroke', 'stylers': [{'color': '#000000'}, {'lightness': 17}, {'weight': 1.2}]}, {'featureType': 'landscape', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 20}]}, {'featureType': 'poi', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 21}]}, {'featureType': 'road.highway', 'elementType': 'geometry.fill', 'stylers': [{'color': '#000000'}, {'lightness': 17}]}, {'featureType': 'road.highway', 'elementType': 'geometry.stroke', 'stylers': [{'color': '#000000'}, {'lightness': 29}, {'weight': 0.2}]}, {'featureType': 'road.arterial', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 18}]}, {'featureType': 'road.local', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 16}]}, {'featureType': 'transit', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 19}]}, {'featureType': 'water', 'elementType': 'geometry', 'stylers': [{'color': '#000000'}, {'lightness': 17}]}];
+  // var styledMap = new google.maps.StyledMapType(styles,
+  //   {name: 'Styled Map'});
+  var bangalore = { lat: 41.8953478, lng: -87.627070 };
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
-    scrollwheel: false,
+    // scrollwheel: false,
     center: bangalore,
-    disableDefaultUI: true
+    // disableDefaultUI: true
   });
-  map.mapTypes.set('map_style', styledMap);
-  map.setMapTypeId('map_style');
+  // map.mapTypes.set('map_style', styledMap);
+  // map.setMapTypeId('map_style');
   addMarker(bangalore, map);
 }
 
@@ -30,6 +30,7 @@ function addMarker(location, map) {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
 $('.nav-item').click(function(){
   'use strict';
