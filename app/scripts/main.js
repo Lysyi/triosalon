c4ac60617561f192380907ed59d9e44d23eaf8bc
Andriy Drozdenko <andriy_drozdenko2@epam.com>
2016-01-07 00:47:44 +0200

    js refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,8 +14,8 @@ var gmarkers = [];
 var htmls = [];
 
 // arrays to hold variants of the info window html with get direction forms open
-var to_htmls = [];
-var from_htmls = [];
+var toHtmls = [];
+var fromHtmls = [];
 
 // global "map" variable
 var map = null;
@@ -26,7 +26,7 @@ var infowindow = new google.maps.InfoWindow({
 
 
 function initialize() {
-
+  'use strict';
   var location = new google.maps.LatLng(41.8953478, -87.627070);
 
   var mapOptions = {
@@ -35,11 +35,11 @@ function initialize() {
     scrollwheel: false
   };
 
-  map = new google.maps.Map(document.getElementById("map"),
+  map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 
   directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+  directionsDisplay.setPanel(document.getElementById('directionsPanel'));
   google.maps.event.addListener(map, 'click', function() {
     infowindow.close();
   });
@@ -55,7 +55,7 @@ function initialize() {
   var latlng = location;
 
   // The info window version with the "to here" form open
-  to_htmls[i] = html + '<br>Directions: <b>To here<\/b> - <a href="javascript:fromhere(' + i + ')">From here<\/a>' +
+  toHtmls[i] = html + '<br>Directions: <b>To here<\/b> - <a href="javascript:fromhere(' + i + ')">From here<\/a>' +
     '<br>Start address:<form action="javascript:getDirections()">' +
     '<input type="text" SIZE=40 MAXLENGTH=40 name="saddr" id="saddr" value="" /><br>' +
     '<INPUT value="Get Directions" TYPE="button" onclick="getDirections()"><br>' +
@@ -63,7 +63,7 @@ function initialize() {
     '<input type="hidden" id="daddr" value="' + latlng.lat() + ',' + latlng.lng() +
     '"/>';
   // The info window version with the "from here" form open
-  from_htmls[i] = html + '<br>Directions: <a href="javascript:tohere(' + i + ')">To here<\/a> - <b>From here<\/b>' +
+  fromHtmls[i] = html + '<br>Directions: <a href="javascript:tohere(' + i + ')">To here<\/a> - <b>From here<\/b>' +
     '<br>End address:<form action="javascript:getDirections()">' +
     '<input type="text" SIZE=40 MAXLENGTH=40 name="daddr" id="daddr" value="" /><br>' +
     '<INPUT value="Get Directions" TYPE="SUBMIT"><br>' +
@@ -89,38 +89,39 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 // ===== request the directions =====
 function getDirections() {
+  'use strict';
   // ==== Set up the walk and avoid highways options ====
   var request = {};
-  if (document.getElementById("walk").checked) {
+  if (document.getElementById('walk').checked) {
     request.travelMode = google.maps.DirectionsTravelMode.WALKING;
   } else {
     request.travelMode = google.maps.DirectionsTravelMode.DRIVING;
   }
 
-  if (document.getElementById("highways").checked) {
+  if (document.getElementById('highways').checked) {
     request.avoidHighways = true;
   }
   // ==== set the start and end locations ====
-  var saddr = document.getElementById("saddr").value;
-  var daddr = document.getElementById("daddr").value;
+  var saddr = document.getElementById('saddr').value;
+  var daddr = document.getElementById('daddr').value;
 
   request.origin = saddr;
   request.destination = daddr;
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
-    } else alert("Directions not found:" + status);
+    } else alert('Directions not found:' + status);
   });
 }
 
 // functions that open the directions forms
 function tohere(i) {
-  infowindow.setContent(to_htmls[i]);
+  infowindow.setContent(toHtmls[i]);
   infowindow.open(map, gmarkers[i]);
 }
 
 function fromhere(i) {
-  infowindow.setContent(from_htmls[i]);
+  infowindow.setContent(fromHtmls[i]);
   infowindow.open(map, gmarkers[i]);
 }
 
